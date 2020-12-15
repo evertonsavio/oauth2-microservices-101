e6c4d0f7d7f5440cce1f754e3081fcd61b7fe0de
Everton Savio Santos Lucas <everluca@outlook.com>
2020-12-15 10:44:17 -0300

    :tada:keycloak spi for access ms-users

---
+++ MyRemoteUserStorageProvider/README.md
@@ -0,0 +1,6 @@
+### Keycloak SPI  
+  
+* On maven repository look for: <b> keycloak server spi </b>
+* Maybe it is necessary to add: <b> keycloak </b> core dependency.
+
+
---
+++ MyRemoteUserStorageProvider/src/test/java/dev/evertonsavio/keycloak/MyremoteuserstorageproviderApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.evertonsavio.keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyremoteuserstorageproviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
