12bbf88a16fe7954648670074f8bb94395c5df44
havyx <everluca@outlook.com>
2020-12-20 18:16:24 -0300

    :sparkles: pre post secured annotations

--- ResourceServer/src/main/java/evertonsavio/webservice/api/ResourceServer/controllers/UsersController.java
+++ ResourceServer/src/main/java/evertonsavio/webservice/api/ResourceServer/controllers/UsersController.java
@@ -1,9 +1,15 @@
 package evertonsavio.webservice.api.ResourceServer.controllers;
 
 import evertonsavio.webservice.api.ResourceServer.model.User;
+import evertonsavio.webservice.api.ResourceServer.model.UserRest;
 import evertonsavio.webservice.api.ResourceServer.services.KeycloakAdminClientService;
 import org.keycloak.representations.account.UserRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,6 +19,20 @@ public class UsersController {
     @GetMapping("/status")
     public String status(){
         return "working";
+    }
+
+    //@Secured("ROLE_developer")
+    //@PreAuthorize("hasRole('developer')")
+    @PreAuthorize("hasAuthority('ROLE_developer') or #id == #jwt.subject")
+    @DeleteMapping(path = "/{id}")
+    public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt){
+        return "Deleted user id" + id + "and jwt sub = " + jwt.getSubject();
+    }
+
+    @PostAuthorize("returnObject.userId == #jwt.subject")
+    @GetMapping(path = "/{id}")
+    public UserRest getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt){
+        return new UserRest("Savio", "Lucas", "");
     }
 
     @Autowired
